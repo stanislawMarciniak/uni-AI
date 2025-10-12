4787ed39e90804feb9e1c334d066bf44162095f7
Stanisław Marciniak <marciniak.dev@gmail.com>
2025-10-12 23:29:57 +0200

    main denormalize

--- Task2/web/app/main.py
+++ Task2/web/app/main.py
@@ -18,19 +18,26 @@ def drawing():
 # Handle POST request
 @app.route('/', methods=['POST'])
 def canvas():
+    # Recieve base64 data from the user form
     canvasdata = request.form['canvasimg']
-    encoded_data = canvasdata.split(',')[1]
+    encoded_data = request.form['canvasimg'].split(',')[1]
 
-    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    # Decode base64 image to python array
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+    # Convert 3 channel image (RGB) to 1 channel image (GRAY)
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray_image = cv2.bitwise_not(gray_image)  # <--- important
-    gray_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_AREA)
-    gray_image = gray_image / 255.0  # normalize
 
+    # Resize to (28, 28)
+    gray_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_LINEAR)
+
+    # Expand to numpy array dimenstion to (1, 28, 28)
     img = np.expand_dims(gray_image, axis=0)
 
-    prediction = np.argmax(model.predict(img), axis=1)[0]
-    print(f"Prediction Result: {prediction}")
-    return render_template('drawing.html', response=str(prediction), canvasdata=canvasdata, success=True)
+    try:
+        prediction = np.argmax(model.predict(img))
+        print(f"Prediction Result : {str(prediction)}")
+        return render_template('drawing.html', response=str(prediction), canvasdata=canvasdata, success=True)
+    except Exception as e:
+        return render_template('drawing.html', response=str(e), canvasdata=canvasdata)
